5062eaf5ec2cc37d4cd84a8297c7d1d02d660d6d
Wouter0100 <wouter@wouter0100.nl>
2015-12-09 13:31:13 +0100

    Implemented the library and started testing, requesting certificate works.

--- da_letsencrypt/includes/bootstrap.php
+++ da_letsencrypt/includes/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace LetsEncrypt;
+//foreach (glob(__DIR__ . '/lib/*') as $file) {
+//    require_once $file;
+//}
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
@@ -9,3 +11,18 @@ ini_set('error_log', dirname(__DIR__) . '/logs/php-error.log');
 
 parse_str(getenv('QUERY_STRING'));
 parse_str(getenv('POST'));
+
+$accountPath = $_SERVER['HOME'];
+$accountSettingsPath = $accountPath . DIRECTORY_SEPARATOR . '.letsencrypt';
+if (!file_exists($accountSettingsPath)) {
+    mkdir($accountSettingsPath);
+}
+
+if (isset($domain)) {
+    $domainPath = $_SERVER['HOME']  . DIRECTORY_SEPARATOR . 'domains' . DIRECTORY_SEPARATOR . $domain;
+    $domainSettingsPath = $domainPath . DIRECTORY_SEPARATOR . '.letsencrypt';
+
+    if (!file_exists($domainSettingsPath)) {
+        mkdir($domainSettingsPath);
+    }
+}
