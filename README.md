cbc1eabbdd36ed3295e13882f3e5fc084b94da4e
Wouter0100 <wouter@wouter0100.nl>
2015-12-10 12:09:32 +0100

    Created version script which receives last version

---
+++ README.md
@@ -0,0 +1,3 @@
+# Versions script for DirectAdmin Let's Encrypt
+
+This branch contains the scripts to automatically deliver and parse the latest version and `.tar.gz`-download to DirectAdmin servers.
---
+++ version.php
@@ -0,0 +1,27 @@
+<?php
+
+$headers = get_headers('https://github.com/Petertjuh360/da-letsencrypt/releases/latest');
+
+foreach ($headers as $header) {
+    $header = explode(': ', $header, 2);
+
+    if ($header[0] == 'Location') {
+        $location = $header[1];
+    }
+}
+
+$tagFound = false;
+$version = null;
+
+foreach (explode('/', $location) as $locationPart) {
+    if ($version == null && $tagFound == true) {
+        $version = $locationPart;
+        break;
+    }
+
+    if ($locationPart == 'tag') {
+        $tagFound = true;
+    }
+}
+
+echo preg_replace("/[^.0-9]/", '', $version);
