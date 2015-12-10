9e536437085af00c9e8e75a9b6c16905dc3ed858
Wouter van Os <wouter@wouter0100.nl>
2015-12-10 21:06:28 +0100

    Load composer after php log path has been set

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-
 ini_set('log_errors', 1);
 ini_set('error_log', dirname(__DIR__) . '/logs/php-error.log');
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 parse_str(getenv('QUERY_STRING'));
 parse_str(getenv('POST'));
