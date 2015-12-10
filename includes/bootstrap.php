54d7adee84a476b803d664f847b3ce815add6da1
Wouter van Os <wouter@wouter0100.nl>
2015-12-10 21:12:24 +0100

    Updated parse to $_GET and $_POST global variables

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,10 +1,11 @@
 <?php
 
+global $_POST, $_GET;
+
 ini_set('log_errors', 1);
 ini_set('error_log', dirname(__DIR__) . '/logs/php-error.log');
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-parse_str(getenv('QUERY_STRING'));
-parse_str(getenv('POST'));
-
+parse_str(getenv('QUERY_STRING'), $_GET);
+parse_str(getenv('POST'), $_POST);
