6add84ba533189d501da0fc1c5d950c8e92699d1
Wouter van Os <wouter@wouter0100.nl>
2015-12-10 21:08:07 +0100

    Forgot to push storage trait

---
+++ includes/lib/Utility/StorageTrait.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace DirectAdmin\LetsEncrypt\Lib\Utility;
+
+/**
+ * Class StorageTrait
+ *
+ * @method string getStoragePath
+ *
+ * @package DirectAdmin\LetsEncrypt\Lib\Utility
+ */
+trait StorageTrait {
+
+    /**
+     * Write to the storage, always overwrites files
+     *
+     * @param $fileName
+     * @param $content
+     */
+    public function writeToStorage($fileName, $content) {
+        $path = $this->getStoragePath();
+
+        if (!$this->existsInStorage($path)) {
+            mkdir($path);
+        }
+
+        file_put_contents($path . DIRECTORY_SEPARATOR . $fileName, $content);
+    }
+
+    /**
+     * Get from storage
+     *
+     * @param $fileName
+     *
+     * @return string
+     */
+    public function getFromStorage($fileName) {
+        return file_get_contents($this->getStoragePath() . DIRECTORY_SEPARATOR . $fileName);
+    }
+
+    /**
+     * Check if a file or folder exists
+     *
+     * @param $fileName
+     *
+     * @return string
+     */
+    public function existsInStorage($fileName) {
+        return file_exists($this->getStoragePath() . DIRECTORY_SEPARATOR . $fileName);
+    }
+}
