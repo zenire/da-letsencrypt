ac8c41fac09614655fd94b374c451093bc91f9e1
Wouter van Os <wouter@wouter0100.nl>
2015-12-11 21:29:34 +0100

    Remove domain from URL

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -2,6 +2,8 @@
 
 global $_POST, $_GET;
 
+use DirectAdmin\LetsEncrypt\Lib\Logger;
+
 ini_set('log_errors', 1);
 ini_set('error_log', dirname(__DIR__) . '/logs/php-error.log');
 
@@ -9,3 +11,8 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 parse_str(getenv('QUERY_STRING'), $_GET);
 parse_str(getenv('POST'), $_POST);
+
+if (!isset($_SERVER['SESSION_SELECTED_DOMAIN']) || empty($_SERVER['SESSION_SELECTED_DOMAIN'])) {
+    $log = new Logger();
+    $log->error('Please select a domain first at the DirectAdmin homepage.');
+}
