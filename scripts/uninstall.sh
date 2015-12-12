3b77354ef190a74991ab12d88a6af0228fa97fec
Wouter van Os <wouter@wouter0100.nl>
2015-12-12 17:29:07 +0100

    Delete cronjob on uninstall

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -6,5 +6,8 @@
 PLUGINPATH=/usr/local/directadmin/plugins/da_letsencrypt
 rm -rf ${PLUGINPATH}
 
+# Delete cronjob
+rm -fr /etc/cron.d/letsencrypt
+
 echo "Plugin is removed from DirectAdmin! Note: SSL certificates managed by this plugin isn\'t removed.";
 exit 0;
