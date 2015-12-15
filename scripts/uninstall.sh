47fa2154e18bbc5e80e38a30c86fd1fcb918707b
Petertjuh360 <Petertjuh360@users.noreply.github.com>
2015-12-15 12:01:45 +0100

    Fixed typo, simpler logic and improved comments

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
-#
-#Created by Peter Bin
 
-#Plugin
-PLUGINPATH=/usr/local/directadmin/plugins/da_letsencrypt
-rm -rf ${PLUGINPATH}
+#Remove plugin
+rm -rf /usr/local/directadmin/plugins/da_letsencrypt
 
-# Delete cronjob
-rm -fr /etc/cron.d/letsencrypt
+#Remove cronjob
+rm -rf /etc/cron.d/letsencrypt
 
 echo "Plugin is removed from DirectAdmin! Note: SSL certificates managed by this plugin isn\'t removed.";
 exit 0;
