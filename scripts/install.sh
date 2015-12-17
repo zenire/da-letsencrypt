0a7fd76842f181a823f0e7129190fe432ae5ada2
Petertjuh360 <Petertjuh360@users.noreply.github.com>
2015-12-17 13:51:39 +0100

    Remove composer install - issue #30

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,14 +5,6 @@
 PLUGINPATH=/usr/local/directadmin/plugins/da-letsencrypt
 cd ${PLUGINPATH}
 
-# Install dependencies using Composer
-if [ ! -d ${PLUGINPATH}/vendor ]; then
-    curl -sS https://getcomposer.org/installer -o /tmp/composer_installer
-    php /tmp/composer_installer --quiet --install-dir=/tmp
-    /tmp/composer.phar install -o --prefer-dist -q
-    rm -f /tmp/composer_installer /tmp/composer.phar /tmp/installer
-fi
-
 # Install cronjob
 echo "30 1 * * * root /usr/local/bin/php /usr/local/directadmin/plugins/da-letsencrypt/scripts/cron.php" > /etc/cron.d/letsencrypt
 
