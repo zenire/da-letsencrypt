6a294111f1545e22a9a8fbe72e3eec1e48f0937b
Wouter0100 <wouter@wouter0100.nl>
2015-12-18 23:18:28 +0100

    Revert "Temporary note for cronjob #36"
    This reverts commit 239e1737563235e14aec2060c2940f9d8624cd53.

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -6,5 +6,5 @@ rm -rf /usr/local/directadmin/plugins/da_letsencrypt
 #Remove cronjob
 rm -rf /etc/cron.d/letsencrypt
 
-echo "Plugin is removed from DirectAdmin! Note: SSL certificates managed by this plugin aren\'t removed. SSL certificates installed by this plugin will not be automatically renewed anymore. Check the expire date of the SSL certificates to prevent using an invalid SSL certificate after 90 days. Also you need to remove the cronjob installed by this plugin.";
+echo "Plugin is removed from DirectAdmin! Note: SSL certificates managed by this plugin aren\'t removed. SSL certificates installed by this plugin will not be automatically renewed anymore. Check the expire date of the SSL certificates to prevent using an invalid SSL certificate after 90 days.";
 exit 0;
