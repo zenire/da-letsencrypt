7623535316fa75c01dd00f024224df13d4f17b09
Wouter0100 <wouter@wouter0100.nl>
2016-01-22 16:48:40 +0100

    Switched if statement

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -15,8 +15,8 @@ if (!defined('CRON')) {
     parse_str(getenv('QUERY_STRING'), $_GET);
     parse_str(getenv('POST'), $_POST);
 
-    if (!isset($_SERVER['SESSION_SELECTED_DOMAIN']) || empty($_SERVER['SESSION_SELECTED_DOMAIN'])) {
-        if ($_SERVER['RUNNING_AS'] != 'admin') {
+    if ($_SERVER['RUNNING_AS'] != 'admin') {
+        if (!isset($_SERVER['SESSION_SELECTED_DOMAIN']) || empty($_SERVER['SESSION_SELECTED_DOMAIN'])) {
             $log = new Logger();
             $log->error('Please select a domain first at the DirectAdmin homepage.');
         }
